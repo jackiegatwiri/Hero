4dc829178f7b1deb0d40af0902e31f858280ef27
jackiegatwiri <jaciegatz@gmail.com>
2019-04-13 15:07:49 +0300

    test for getAge

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -7,4 +7,11 @@ public class HeroTest {
     Hero myHero = new Hero("Superman", 45, "fly", "cryptonite");
     assertTrue(myHero instanceof Hero); //to instantiate an object for the class
     }
+    @Test
+    public void getName_getNameForHeroes_String(){
+        Hero myHero = new Hero("Superman", 45, "fly", "cryptonite");
+        String expected = "Superman";
+        assertEquals(expected,myHero.getName());
+    }
+
 }
