49dae9e8224e1b661bbc29a149bf99ab38ddbc65
jackiegatwiri <jaciegatz@gmail.com>
2019-04-14 12:22:04 +0300

    added a list of heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
@@ -7,6 +9,37 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
         port(9000);
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("heroes", request.session().attribute("heroes"));
+            model.put("template", "templates/index.vtl");
+
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();//Remember, any extra information the template needs must be packaged in a HashMap named model
+           ArrayList<Hero> heroes = request.session().attribute("heroes"); //this arraylist retrieves all your heoers, were heres are being saved of it ain't there create a new one
+
+           if(heroes == null){
+               heroes = new ArrayList<Hero>(); //adds a new arraylist if not yet created
+               request.session().attribute("heroes", heroes);}
+            String name = request.queryParams("name");
+            String age = request.queryParams("age");
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Hero newHero = new Hero(name, Integer.parseInt(age), power, weakness);
+            heroes.add(newHero); //add the new hero to the heroes arraylist
+
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
     }
 }
--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
     private String mName;
     private int mAge;
     private String mPower;
     private String mWeakness;
+    private int mId;
+    private static List<Hero> instances = new ArrayList<>();
 
     public Hero(String name, int age, String power, String weakness) {
         this.mName = name;
         this.mAge = age;
         this.mPower = power;
         this.mWeakness = weakness;
+
+         instances.add(this);
+
     }
 
     public String getName() {
@@ -26,8 +34,18 @@ public class Hero {
     public String getWeakness() {
         return mWeakness;
     }
-
-
+    public int getId(){
+        return mId;
+    }
+    public static List<Hero> all(){
+        return instances;
+    }
+    public static Hero find(int id) {
+        return instances.get(id - 1);
+    }
+    public static void clear() {
+        instances.clear();
+    }
 
 
 
