f7a5b8556950fd06dc6fe9fdbe26a161bec95811
jackiegatwiri <jaciegatz@gmail.com>
2019-04-14 17:41:06 +0300

    test for instantiate with name

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,7 +1,11 @@
 public class Squad {
+
     private String mName;
 
     public Squad(String name ){
         this.mName = name;
+    }
+    public String getName() {
+        return mName;
     }
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -7,4 +7,9 @@ public class SquadTest {
         Squad testSquad = new Squad("Epic");
         assertTrue(testSquad instanceof Squad);
     }
+    @Test
+    public void Squad_instantiateWithName_Epic(){
+        Squad testSquad = new Squad("Epic");
+        assertEquals("Epic", testSquad.getName());
+    }
 }
