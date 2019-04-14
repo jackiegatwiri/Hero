ed5b2aab024e70e3b3adef17cd525ed2acbbf5dd
jackiegatwiri <jaciegatz@gmail.com>
2019-04-14 17:50:40 +0300

    test all of users Squad category

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
 
     private String mName;
+    private static List<Squad> instances = new ArrayList<Squad>(); //list of category objects
 
     public Squad(String name ){
         this.mName = name;
+        instances.add(this);
     }
     public String getName() {
         return mName;
     }
+    public static List<Squad> all() {
+        return instances;
+    }
+
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -12,4 +12,15 @@ public class SquadTest {
         Squad testSquad = new Squad("Epic");
         assertEquals("Epic", testSquad.getName());
     }
+
+    @Test
+    public void all_returnsAllInstanceOfSquad_true(){
+        Squad firstSquad = new Squad("Epic");
+        Squad secondSquad = new Squad("AntiHero");
+        Squad thirdSquad = new Squad("Classical");
+
+        assertEquals(true, Squad.all().contains(firstSquad));
+        assertEquals(true, Squad.all().contains(secondSquad));
+        assertEquals(true, Squad.all().contains(thirdSquad));
+    }
 }
